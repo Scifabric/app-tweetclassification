a40588b7096c4ec71428795853255747608ffb9f
Daniel Lombraña González <teleyinex@gmail.com>
2012-12-05 17:36:25 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+PyBossa application for classifying tweets
+==========================================
+
+This small app shows how you can classify tweets really easily from a CSV
+file.
---
+++ createTasks.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of PyBOSSA.
+#
+# PyBOSSA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyBOSSA is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with PyBOSSA.  If not, see <http://www.gnu.org/licenses/>.
+
+import urllib
+import urllib2
+import json
+import re
+import string
+from optparse import OptionParser
+import pbclient
+
+def task_formatter(app_config, row, n_answers):
+    """
+    Creates tasks for the application
+
+    :arg integer app_id: Application ID in PyBossa.
+    :returns: Task ID in PyBossa.
+    :rtype: integer
+    """
+    # Each row has the following format
+    # row[0] = teetid,
+    # row[1] = text,
+    # row[2] = date
+    # row[3] = username
+    # row[4] = userid
+
+    info = dict(question=app_config['question'],
+                n_answers=int(n_answers),
+                tweetid = row[0],
+                text = row[1],
+                date = row[2],
+                username = row[3],
+                userid = row[4],
+                )
+
+    return info
+
+if __name__ == "__main__":
+    # Arguments for the application
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage)
+    # URL where PyBossa listens
+    parser.add_option("-u", "--url", dest="api_url",
+                      help="PyBossa URL http://domain.com/", metavar="URL")
+    # API-KEY
+    parser.add_option("-k", "--api-key", dest="api_key",
+                      help="PyBossa User API-KEY to interact with PyBossa",
+                      metavar="API-KEY")
+    # Create App
+    parser.add_option("-c", "--create-app", action="store_true",
+                      dest="create_app",
+                      help="Create the application",
+                      metavar="CREATE-APP")
+    # Update template for tasks and long_description for app
+    parser.add_option("-t", "--update-template", action="store_true",
+                      dest="update_template",
+                      help="Update Tasks template",
+                      metavar="UPDATE-TEMPLATE"
+                     )
+
+    # Update tasks question
+    parser.add_option("-q", "--update-tasks", action="store_true",
+                      dest="update_tasks",
+                      help="Update Tasks question",
+                      metavar="UPDATE-TASKS"
+                     )
+
+    parser.add_option("-x", "--extra-task", action="store_true",
+                      dest="add_more_tasks",
+                      help="Add more tasks",
+                      metavar="ADD-MORE-TASKS"
+                      )
+    # Modify the number of TaskRuns per Task
+    # (default 30)
+    parser.add_option("-n", "--number-answers",
+                      dest="n_answers",
+                      help="Number of answers per task",
+                      metavar="N-ANSWERS"
+                     )
+
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose")
+    (options, args) = parser.parse_args()
+
+    # Load app details
+    try:
+        app_json = open('app.json')
+        app_config = json.load(app_json)
+        app_json.close()
+    except IOError as e:
+        print "app.json is missing! Please create a new one"
+        exit(0)
+
+    if not options.api_url:
+        options.api_url = 'http://localhost:5000/'
+    pbclient.set('endpoint', options.api_url)
+
+    if not options.api_key:
+        parser.error("You must supply an API-KEY to create an \
+                      applicationa and tasks in PyBossa")
+    else:
+        pbclient.set('api_key', options.api_key)
+
+    if (options.verbose):
+        print('Running against PyBosssa instance at: %s' % options.api_url)
+        print('Using API-KEY: %s' % options.api_key)
+
+    if not options.n_answers:
+        options.n_answers = 2
+
+    if options.create_app:
+        import csv
+        pbclient.create_app(app_config['name'],
+                app_config['short_name'],
+                app_config['description'])
+        app = pbclient.find_app(short_name=app_config['short_name'])[0]
+        app.long_description = open('long_description.html').read()
+        app.info['task_presenter'] = open('template.html').read()
+        app.info['thumbnail'] = app_config['thumbnail']
+        app.info['tutorial'] = open('tutorial.html').read()
+
+        pbclient.update_app(app)
+        with open('PabloPh_UN_latest.csv', 'rb') as csvfile:
+            csvreader = csv.reader(csvfile, delimiter=',')
+            # Each row has the following format
+            # tweetid,
+            # text
+            # date
+            # username
+            # userid
+            for row in csvreader:
+                if row[0]!='tweetid':
+                    task_info = task_formatter(app_config, row, options.n_answers)
+                    pbclient.create_task(app.id, task_info)
+    else:
+        if options.add_more_tasks:
+            import csv
+            with open('PabloPh_UN_latest.csv', 'rb') as csvfile:
+                csvreader = csv.reader(csvfile, delimiter=',')
+                # Each row has the following format
+                # tweetid,
+                # text
+                # date
+                # username
+                # userid
+                for row in csvreader:
+                    if row[0]!='tweetid':
+                        task_info = task_formatter(app_config, row, options.n_answers)
+                        pbclient.create_task(app.id, task_info)
+
+    if options.update_template:
+        print "Updating app template"
+        app = pbclient.find_app(short_name=app_config['short_name'])[0]
+        app.long_description = open('long_description.html').read()
+        app.info['task_presenter'] = open('template.html').read()
+        app.info['tutorial'] = open('tutorial.html').read()
+        pbclient.update_app(app)
+
+    if options.update_tasks:
+        print "Updating task question"
+        app = pbclient.find_app(short_name=app_config['short_name'])[0]
+        for task in pbclient.get_tasks(app.id):
+            task.info['question'] = u'Ves un humano?'
+            pbclient.update_task(task)
+
+    if not options.create_app and not options.update_template\
+            and not options.add_more_tasks and not options.update_tasks:
+        parser.error("Please check --help or -h for the available options")
